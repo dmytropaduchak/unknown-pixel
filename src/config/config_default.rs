f19b89fd41c163e76c99c023dcd69c75975dfa01
Dmytro Paduchak <dmytro.paduchak@gmail.com>
2025-06-19 12:01:00 +0100

    fix

--- src/config/config_default.rs
+++ src/config/config_default.rs
@@ -1,19 +1,40 @@
-use super::BIG;
-use super::MEDIUM;
-use super::SMALL;
 use miniquad::conf::Conf;
 use miniquad::conf::Icon;
+use std::error::Error;
+use std::fs;
+
+fn icon<const SIZE: usize>(path: &str) -> Result<[u8; SIZE], Box<dyn Error>> {
+    let data = fs::read(path).map_err(|e| format!("Failed to read {}: {}", path, e))?;
+    let len = data.len();
+    data.try_into().map_err(|_| {
+        format!(
+            "{} has invalid size (expected {} bytes, got {})",
+            path, SIZE, len
+        )
+        .into()
+    })
+}
+
+fn files() -> Result<Icon, Box<dyn Error>> {
+    let small = icon::<{ 16 * 16 * 4 }>("icon_16.rgba")?;
+    let medium = icon::<{ 32 * 32 * 4 }>("icon_32.rgba")?;
+    let big = icon::<{ 64 * 64 * 4 }>("icon_64.rgba")?;
+    Ok(Icon { small, medium, big })
+}
 
 pub fn default() -> Conf {
-    let icon = Icon {
-        big: BIG,
-        small: SMALL,
-        medium: MEDIUM,
+    let icon = match files() {
+        Ok(icon) => Some(icon),
+        Err(e) => {
+            eprintln!("Failed to load icons: {e}");
+            None
+        }
     };
-    let conf = Conf {
-        window_title: "unkNOWn Pixel".to_string(),
-        icon: Some(icon),
+    let window_title = "unkNOWn Shape".to_string();
+
+    Conf {
+        window_title,
+        icon,
         ..Default::default()
-    };
-    conf
+    }
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,5 +1,2 @@
 mod config_default;
 pub use config_default::*;
-
-mod config_default_icon;
-pub use config_default_icon::*;
